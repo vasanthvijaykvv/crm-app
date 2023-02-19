159c1646cf37a57367e9779b3d3ca9a2f702032d
Vasanth Vijay K <101649328+vasanthvijaykvv@users.noreply.github.com>
2023-02-19 15:02:15 +0530

    addedpagination &condition for that importantlogic

--- src/components/Customer/CustomerList/CustomerList.js
+++ src/components/Customer/CustomerList/CustomerList.js
@@ -7,23 +7,34 @@ function CustomerList(){
 
     const [customer, setCustomer] = useState([]);
     const [filteredCustomer,setFilteredCustomer] = useState([]);
-    const [error, setError] = useState("")
-    let[counts,setCounts] = useState({})
+    const [error, setError] = useState("");
+    let[counts,setCounts] = useState({});
+    let [pages, setPages] = useState([]);
+    const [previousNext,setPreviousNext] = useState(1)
     const navigate = useNavigate();
     
     
     useEffect(()=>{
-        fetch("http://localhost:4000/api/customer").then(res=>{
+        //This API shows 100 data per page
+        fetch("http://localhost:4000/api/customer/page/1").then(res=>{
             return res.json()
-        }).then(res=> { setCustomer(res)
-            ;setFilteredCustomer(res);         
+        }).then(res=> { setCustomer(res.records)
+            console.log(res.records);
+            setFilteredCustomer(res.records);  
+            //Creating the Array of pages tho display pagination   
+           let totalPages =Math.ceil(res.totalCount/100 );
+           let arrayOfPages = new Array(totalPages).fill(0)
+           console.log(arrayOfPages)
+            setPages(arrayOfPages)      
         }
          )
         .catch((err)=>{
             setError(err.message)
         })
     },[])
+   
     useEffect(()=>{
+        //this api shows total data in the array
         fetch("http://localhost:4000/api/customer").then(res=>{
             return res.json()
         }).then(res =>{
@@ -55,6 +66,37 @@ function CustomerList(){
         setError(err.message)
     })
    }
+   let LoadpreviousNext = (prenex) =>{
+      if(prenex === "Previous" ){
+        if(previousNext===0 || previousNext===1){
+            Pagination(1)             
+        }else{
+            Pagination(previousNext-1)
+        }
+      }
+      if(prenex === "Next" ){
+        if(pages.length === previousNext){
+            console.log(pages.length,previousNext)
+            Pagination(previousNext)           
+        }else{
+            Pagination(previousNext+1)
+            //using the Pagination function again 
+        }
+      }
+   }
+    
+   let Pagination = (pageNo) =>{
+    //Paginating    
+    setPreviousNext(pageNo)
+    fetch("http://localhost:4000/api/customer/page/" + pageNo).then(res=>{
+            return res.json()
+        }).then(res=> { setCustomer(res.records)
+            console.log("pagination Encounted")
+            console.log(res.records);
+            setFilteredCustomer(res.records);            
+        }
+         )
+   }
    function handleSearch(key){
       if(!key | key.length===0 ){
         setFilteredCustomer(customer)
@@ -66,7 +108,7 @@ function CustomerList(){
       }
         
    }
-  console.log(filteredCustomer)
+  //console.log(filteredCustomer)
     return(<div>
         <Navbar />
         <CustomerDashBoard {...counts}/>
@@ -81,7 +123,7 @@ function CustomerList(){
           {filteredCustomer.length === 0 && <div className="alert alert-warning mt-3" role="alert">
          Unable to Connect {error}
        </div>}
-         { filteredCustomer.length>0 && <table className="table table-striped  ">
+         { filteredCustomer.length>0 && <div><table className="table table-striped  ">
            <thead>
                <tr>
                    <th scope="col">Name</th>
@@ -113,7 +155,25 @@ function CustomerList(){
                    ))
                }              
            </tbody>
-          </table>}
+           
+          </table>
+          <nav aria-label="Page navigation example">
+  <ul className="pagination justify-content-center">
+    <li className="page-item ">
+      <button className="page-link"  onClick={()=>LoadpreviousNext("Previous")}>Previous</button>
+    </li>
+    {pages.map((p,i)=>{
+      return <div key={i}>
+        <li className="page-item" ><button className="page-link" onClick={()=>Pagination(i+1)} >{i+1}</button></li>
+      </div>
+    })}    
+    <li className="page-item">
+      <button className="page-link"  onClick={()=>LoadpreviousNext("Next")} >Next</button>
+    </li>
+  </ul>
+</nav>
+          </div>
+          }
         </div></div>
     )
 }
